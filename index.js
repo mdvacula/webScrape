729b0d511db5ac5cbedbcd1fe5e5ea8b6061a878
Matthew Vacula <Matthew Vacula>
2017-06-23 01:02:45 -0400

    Really rough webScrape of Fortune Website

---
+++ index.js
@@ -0,0 +1,32 @@
+//include request and cheerio
+var request = require('request');
+var cheerio = require('cheerio');
+
+//Base Url for (A's)
+var base_url = "http://archive.fortune.com/magazines/fortune/fortune500_archive/letters/A.html";
+
+//make request using base url
+request(base_url, function(err,resp,html){
+  var info = [];      //array to store data
+  //if no error
+  if(!err){
+    $ = cheerio.load(html); //load html into cheerio
+    //select all items with class tablerow
+    $('#tablerow').each(function(i, elem){
+      //console.log($(elem).children().first().text());
+      //console.log($(elem).children().eq(1).text());
+
+      //push the info into the array first item is company and second is years
+      info.push({
+        company: $(elem).children().first().text().trim(),
+        years: $(elem).children().eq(1).first().text().trim()
+      });
+    });
+    //loop through array to print
+    for(var x=0;x<info.length;x++){
+      console.log("---------------------------");
+      console.log("Company: " + info[x].company);
+      console.log("Years: " + info[x].years + "\n");
+    }
+  }
+});
